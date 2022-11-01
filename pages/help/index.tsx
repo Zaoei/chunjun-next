6b8de5e7ccd53a6075d725b76e0450d41d7fb002
xiaowei <wewoor@gmail.com>
2022-11-01 15:52:34 +0800

    feat: add i18n

--- pages/help/index.tsx
+++ pages/help/index.tsx
@@ -1,5 +1,15 @@
+import { GetStaticPropsContext } from "next";
+
 const HelpUs = () => {
-  return <h1>help us</h1>
+  return <h1 className="text-center">help us</h1>
+}
+
+export async function getStaticProps({ locale }: GetStaticPropsContext) {
+  return {
+    props: {
+      messages: (await import(`../../i18n/${locale}.json`)).default
+    }
+  };
 }
 
 export default HelpUs
